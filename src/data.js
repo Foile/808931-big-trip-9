77930d0800af811c646f402f10feb47b93133ac4
foile <foile@mail.ru>
2019-08-20 15:33:26 +0300

    Личный проект: и ехать и шашечки

--- src/data.js
+++ src/data.js
@@ -1,15 +1,3 @@
-const lorem = [` Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra.`,
-  `Aliquam id orci ut lectus varius viverra.`,
-  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
-  `Aliquam erat volutpat.`,
-  `Nunc fermentum tortor ac porta dapibus.`,
-  `In rutrum ac purus sit amet tempus.`];
-
 const eventTitles = [
   [`flight`, `transfer`],
   [`train`, `transfer`],
@@ -30,7 +18,6 @@ export const eventTypeGroups = eventTitles.reduce(
       }),
     {}
 );
-
 
 export const eventType = [
   {title: `flight`, type: `transfer`},

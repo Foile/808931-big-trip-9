0cb0923e30bb0de8bdc4160a1816910206a91aed
foile <foile@mail.ru>
2019-08-30 00:50:06 +0300

    Личный проект: запомни и пересчитай

--- src/components/sort.js
+++ src/components/sort.js
@@ -4,14 +4,12 @@ export class Sort extends AbstractComponent {
   getTemplate() {
     return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     <span class="trip-sort__item  trip-sort__item--day">Day</span>
-
     <div class="trip-sort__item  trip-sort__item--event">
-      <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked="">
+      <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked=""  data-sort-type="event">
       <label class="trip-sort__btn" for="sort-event">Event</label>
     </div>
-
-    <div class="trip-sort__item  trip-sort__item--time">
-      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+    <div class="trip-sort__item  trip-sort__item--time" >
+      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="time">
       <label class="trip-sort__btn" for="sort-time">
         Time
         <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
@@ -19,9 +17,8 @@ export class Sort extends AbstractComponent {
         </svg>
       </label>
     </div>
-
-    <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+    <div class="trip-sort__item  trip-sort__item--price" >
+      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="price">
       <label class="trip-sort__btn" for="sort-price">
         Price
         <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
@@ -29,8 +26,7 @@ export class Sort extends AbstractComponent {
         </svg>
       </label>
     </div>
-
-    <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+    <span class="trip-sort__item  trip-sort__item--offers" >Offers</span>
   </form>`;
   }
 }
--- src/components/trip-controller.js
+++ src/components/trip-controller.js
@@ -9,55 +9,98 @@ import {Sort} from './sort';
 export class TripController {
   constructor(events, container) {
     this._events = events;
+    this._originEvents = Object.assign(events);
     this._container = container;
     this._sort = new Sort();
-    this._eventsList = new EventList();
+    // this._eventsList = new EventList();
+    this._days = new TripDayList();
   }
-  init() {
-    const tripDaysElement = this._events.length > 0 ? new TripDayList().getElement() : new EmptyEventList().getElement();
+
+  _onSortLinkClick(evt) {
+    console.log(evt.target);
+    evt.preventDefault();
+    console.log(evt.target);
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+    this._container.getElement().innerHTML = ``;
+    this._events = this._originEvents;
+
+    switch (evt.target.dataset.sortType) {
+      case `time`:
+        this._events = this._events.slice().sort((a, b) => (a.timeEnd - a.timeStart) - (b.timeEnd - b.timeStart));
+        // this._updateEvents(this._events);
+        this._events.forEach((event)=> {
+          this._renderEvent(this._container.getElement(), event);
+        });
+        break;
+      case `price`:
+        this._events = this._events.slice().sort((a, b) => a.price - b.price);
+        this._events.forEach((event)=> {
+          this._renderEvent(this._container.getElement(), event);
+        });
+        break;
+      case `event`:
+        this._updateEvents(this._events);
+        break;
+    }
+  }
+
+  _renderEvent(container, event) {
+    let eventComponent = new Event(event);
+    let eventEditComponent = new EventEdit(event);
+    const activateView = () => {
+      container.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    };
+    const activateEdit = () => {
+      container.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+      document.addEventListener(`keydown`, onEscKeyDown);
+    };
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        activateView();
+      }
+    };
+    eventComponent.getElement()
+      .querySelector(`.event__rollup-btn`)
+      .addEventListener(`click`, () => {
+        activateEdit();
+      });
+    eventEditComponent.getElement()
+      .querySelector(`.event__rollup-btn`)
+      .addEventListener(`click`, () => {
+        activateView();
+        document.addEventListener(`keydown`, onEscKeyDown);
+      });
+    eventEditComponent.getElement()
+      .querySelector(`.event__save-btn`)
+      .addEventListener(`click`, () => {
+        container.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+    render(container, eventComponent.getElement());
+  }
+
+  _updateEvents(events) {
+    const tripDaysElement = events.length > 0 ? this._days.getElement() : new EmptyEventList().getElement();
     render(this._container, tripDaysElement);
     const days = new Set(this._events.map(({timeStart})=>(new Date(timeStart)).setHours(0, 0, 0, 0)));
 
     Array.from(days).forEach((day, index) => {
+      const eventContainer = new EventList();
       let dayElement = new TripDay(day, index + 1).getElement();
       render(tripDaysElement, dayElement);
-      render(dayElement, this._eventsList.getElement());
-
-      this._events.filter(({timeStart}) => new Date(day).toLocaleDateString() === new Date(timeStart).toLocaleDateString()).forEach((event)=> {
-        let eventComponent = new Event(event);
-        let eventEditComponent = new EventEdit(event);
-
-        const onEscKeyDown = (evt) => {
-          if (evt.key === `Escape` || evt.key === `Esc`) {
-            this._eventsList.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
-            document.removeEventListener(`keydown`, onEscKeyDown);
-          }
-        };
-
-        eventComponent.getElement()
-          .querySelector(`.event__rollup-btn`)
-          .addEventListener(`click`, () => {
-            this._eventsList.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
-            document.addEventListener(`keydown`, onEscKeyDown);
-          });
-
-        eventEditComponent.getElement()
-          .querySelector(`.event__rollup-btn`)
-          .addEventListener(`click`, () => {
-            this._eventsList.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
-            document.addEventListener(`keydown`, onEscKeyDown);
-          });
-
-        eventEditComponent.getElement()
-          .querySelector(`.event__save-btn`)
-          .addEventListener(`click`, () => {
-            this._eventsList.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
-            document.removeEventListener(`keydown`, onEscKeyDown);
-          });
-
-
-        render(this._eventsList, eventComponent.getElement());
+      render(dayElement, eventContainer.getElement());
+      events.filter(({timeStart}) => new Date(day).toLocaleDateString() === new Date(timeStart).toLocaleDateString())
+      .forEach((event)=> {
+        this._renderEvent(eventContainer.getElement(), event);
       });
     });
+  }
+  init() {
+    this._updateEvents(this._events);
+    this._sort.getElement()
+    .addEventListener(`click`, (evt) => this._onSortLinkClick(evt));
   }
 }

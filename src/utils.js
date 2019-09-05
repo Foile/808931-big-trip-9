f288240c97f54e1771f60b27f35b1942977d55c8
a.shishova <a.shishova@contact-sys.com>
2019-09-05 18:31:19 +0300

    add delete

--- src/utils.js
+++ src/utils.js
@@ -29,7 +29,7 @@ export const unrender = (Component) => {
     Component.removeElement();
   }
 };
-export const makeFirstSymUp = (string) => `${string[0].toUpperCase()}${string.slice(1)}`;
+export const makeFirstSymUp = (string) => `${string.length > 0 ? string[0].toUpperCase() + string.slice(1) : ``}`;
 
 export const calcDuration = (timeStart, timeEnd) => {
   let diff = Math.abs(timeEnd - timeStart) / 1000;
@@ -45,5 +45,16 @@ export const calcDuration = (timeStart, timeEnd) => {
     diff -= result[key] * partTime[key];
   });
 
-  return Object.keys(result).map((cur) => result[cur] > 0 ? `${result[cur]}${cur[0].toUpperCase()}` : ``).join(` `);
+  return Object.keys(result).map((cur) => result[cur] > 0 ? `${result[cur]} ${cur[0].toUpperCase()} ` : ``).join(` `);
+};
+
+export const calcPrice = (events) => {
+  let totalAmount = 0;
+  events.map(((event) => {
+    totalAmount += event ? Number(event.price) : 0;
+    event.offers.map((offer) => {
+      totalAmount += offer ? Number(offer.price) : 0;
+    });
+  }));
+  return totalAmount;
 };

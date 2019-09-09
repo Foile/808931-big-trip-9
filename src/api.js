ca49e914b0b8193acb7a4e67100897ae6a2eb415
foile <foile@mail.ru>
2019-09-09 10:36:58 +0300

    api

---
+++ src/api.js
@@ -0,0 +1,44 @@
+const Method = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+const Api = class {
+  constructor({endPoint, authorization}) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getEvents() {
+  }
+
+  createEvent({Event}) {
+  }
+
+  updateEvent({id, data}) {
+  }
+
+  deleteEvent({id}) {
+  }
+
+  _load({url, method = Method.GET, body = null, headers = new Headers()}) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(checkStatus)
+      .catch((err) => {
+        console.error(`fetch error: ${err}`);
+        throw err;
+      });
+  }
+};
